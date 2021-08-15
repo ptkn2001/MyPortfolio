dabb244be46f78f8105651c39d228802fdc97f59
Derrick Nguyen <ptkn2001@msn.com>
2021-08-15 13:35:08 -0700

    add javascript

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const contentMainEl = document.querySelector('#work_content_main');
+const content2El = document.querySelector('#work_content_2');
+const content3El = document.querySelector('#work_content_3');
+
+contentMainEl.addEventListener('click', event => {
+    let url = "https://troyat88.github.io/Find-My-Plate/";
+    window.open(url);
+});
+
+content2El.addEventListener('click', event => {
+    let url = "https://ptkn2001.github.io/MyPortfolio/";
+    window.open(url);
+});
+
+content3El.addEventListener('click', event => {
+    /*TODO: at the later time when this project is completed. */
+});
